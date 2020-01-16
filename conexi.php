ca596d3899ed1daa3c9c0713860f76c3c68f9faf
ferchuse <sistemas@glifo.mx>
2020-01-16 12:06:07 -0600

    Login

--- conexi.php
+++ conexi.php
@@ -36,7 +36,7 @@
 			die( "Error seleccionando la base de datos.". mysqli_error($link));
 		}
 		
-		if($_SERVER["SERVER_NAME"] == "estambresatoshka.com"){
+		if($_SERVER["SERVER_NAME"] != "localhost"){
 				mysqli_query($link, "SET CHARACTER SET utf8") or die("Error en charset UTF8".mysqli_error($link));
 		}
 			
