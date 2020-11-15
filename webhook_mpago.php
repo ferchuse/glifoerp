fc1b2b7dbb893a03e314b29090389a6de743f80e
ferchuse <sistemas@glifo.mx>
2020-11-15 13:24:33 -0600

    Update webhook_mpago.php

--- webhook_mpago.php
+++ webhook_mpago.php
@@ -12,7 +12,7 @@
 	$link = Conectarse();
 	
 	// $content = json_encode($_POST);
-	// $content = file_get_contents('php://input');
+	$content = file_get_contents('php://input');
 	
 	$decoded = json_decode($content);
 	
@@ -53,27 +53,44 @@
 		break;
 	}
 	
-    $paid_amount = 0;
-    foreach ($merchant_order->payments as $payment) {
-        if ($payment['status'] == 'approved'){
-            $paid_amount += $payment['transaction_amount'];
-		}
-	}
+    // $paid_amount = 0;
+    // foreach ($merchant_order->payments as $payment) {
+	// if ($payment['status'] == 'approved'){
+	// $paid_amount += $payment['transaction_amount'];
+	// }
+	// }
 	
     // If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
-    if($paid_amount >= $merchant_order->total_amount){
-        if (count($merchant_order->shipments)>0) { // The merchant_order has shipments
-            if($merchant_order->shipments[0]->status == "ready_to_ship") {
-                print_r("Totally paid. Print the label and release your item.");
-			}
-			} else { // The merchant_order don't has any shipments
-            print_r("Totally paid. Release your item.");
-		}
-		} else {
-        print_r("Not paid yet. Do not release your item.");
+    // if($paid_amount >= $merchant_order->total_amount){
+	// if (count($merchant_order->shipments)>0) { // The merchant_order has shipments
+	// if($merchant_order->shipments[0]->status == "ready_to_ship") {
+	// print_r("Totally paid. Print the label and release your item.");
+	// }
+	// } else { // The merchant_order don't has any shipments
+	// print_r("Totally paid. Release your item.");
+	// }
+	// } else {
+	// print_r("Not paid yet. Do not release your item.");
+	// }
+	
+	if ($payment->status == 'approved'){
+		$external_reference =$payment->external_reference;
+		
+		
+		$update = "UPDATE cargos SET estatus = 'Pagado' WHERE id_cargos = '{$external_reference}'";
+		
+		$result_update = mysqli_query($link, $update);
+		
+		echo "result".$result_update;
 	}
 	
+	
 	echo "<pre >"; print_r($payment); echo "</pre>";
-	echo "<pre >"; print_r($merchant_order); echo "</pre>";
+	// echo "<pre >"; print_r($merchant_order); echo "</pre>";
+	
+	
+	
+	
+	//Enviar email de pago y desbloquear sistema
 	
 ?>
