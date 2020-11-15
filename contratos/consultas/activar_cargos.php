79da991d4c66678ea87e58e60c60d3ef173eb40e
ferchuse <sistemas@glifo.mx>
2020-11-15 12:45:29 -0600

    Update activar_cargos.php

--- contratos/consultas/activar_cargos.php
+++ contratos/consultas/activar_cargos.php
@@ -1,11 +1,18 @@
 <?php
-	header("Content-Type: application/json");
+	// header("Content-Type: application/json");
 	include ("../../conexi.php");
 	include ("../../lib/mpago/vendor/autoload.php");
 	include ("../../lib/sendinblue/vendor/autoload.php");
 	
 	$link = Conectarse();
 	$respuesta = Array();
+	$lista_cargos = Array();
+	
+	
+	// $token = "TEST-2599583807479799-071201-647536aad8d58734906bd5ae1ea91d39-88027643"; //TEST GLIFO
+	
+	// $token = "APP_USR-2599583807479799-071201-57bd8dadef36a95264421bf038610bd6-88027643"; //PROD GLIFO
+	$token = "TEST-3710248158721318-103003-8334d320761e1338f9c10925c43c1841-665671200"; //TEST MERCHENT
 	
 	$fecha_actual = date("Y-m-d");
 	$fecha_actual = date("2021-01-17");
@@ -36,13 +43,20 @@
 		$respuesta["mensaje"] = "Error $consulta  ".mysqli_error($link);		
 	}
 	
+	echo "<pre>";
+	print_r($lista_cargos);
+	echo "</pre>";
 	
 	//Por cada Cargo crear link y actualizar columna link_pago Enviar correo
+	
+	MercadoPago\SDK::setAccessToken($token);
 	
 	foreach($lista_cargos as $cargo){
 		
 		
-		MercadoPago\SDK::setAccessToken($token);
+		echo $cargo["concepto"]."<br>";
+		echo $cargo["importe"]."<br>";
+		echo $link_pago ;
 		
 		// Crea un objeto de preferencia
 		$preference = new MercadoPago\Preference();
@@ -59,7 +73,7 @@
 		// "pending" => "https://cumbresdemexico.com.mx/2020/web/mi_cuenta.php" 
 		// );
 		
-		$preference->statement_descriptor =  "Cumbre Nacional";
+		$preference->statement_descriptor =   $cargo["concepto"];
 		$preference->external_reference = $cargo["id_cargos"];
 		$preference->auto_return = "approved"; 
 		$preference->items = array($item);
@@ -71,34 +85,36 @@
 		$link_pago =  $preference->init_point;
 		
 		
+		echo $cargo["concepto"]."<br>";
+		echo $cargo["importe"]."<br>";
+		echo $link_pago ;
 		
 		
 		
 		
+		// $consulta = "UPDATE cargos SET 
+		// estatus = 'Pendiente'
+		// link_pago = '$link_pago'
 		
-		$consulta = "UPDATE cargos SET 
-		estatus = 'Pendiente'
-		link_pago = '$link_pago'
+		// WHERE id_cargos = {$cargo["id_cargos"]}
 		
-		WHERE id_cargos = {$cargo["id_cargos"]}
+		// ";
 		
-		";
+		// $result = mysqli_query($link, $consulta);
 		
-		$result = mysqli_query($link, $consulta);
+		// $respuesta["consulta"] = $consulta;
 		
-		$respuesta["consulta"] = $consulta;
-		
-		if($result){
-		$respuesta["status"] = "success";
-		$respuesta["mensaje"] = "Guardado";
-		
-		$folio = mysqli_insert_id($link);
-		
-		}	
-		else{
-		$respuesta["status"] = "error";
-		$respuesta["mensaje"] = "Error $consulta  ".mysqli_error($link);		
-		}
+		// if($result){
+			// $respuesta["status"] = "success";
+			// $respuesta["mensaje"] = "Guardado";
+			
+			// $folio = mysqli_insert_id($link);
+			
+		// }	
+		// else{
+			// $respuesta["status"] = "error";
+			// $respuesta["mensaje"] = "Error $consulta  ".mysqli_error($link);		
+		// }
 		
 		
 		
