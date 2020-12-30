d342c9c84c3ac38abd88491384b4eca29e4ef8d4
ferchuse <sistemas@glifo.mx>
2020-12-30 11:40:14 -0600

    Update get_emisor.php

--- facturacion/emisores/get_emisor.php
+++ facturacion/emisores/get_emisor.php
@@ -5,7 +5,7 @@
 	$link = Conectarse();
 	$respuesta = [];
 	
-	$consulta = "SELECT * FROM emisores WHERE id_emisores = '{$_GET["id_emisores"]}'";
+	$consulta = "SELECT * FROM emisores WHERE id_emisores = '{$_COOKIE["id_emisores"]}'";
 	$respuesta["consulta"] = $consulta;
 	
 	$result = mysqli_query($link,$consulta) ;
